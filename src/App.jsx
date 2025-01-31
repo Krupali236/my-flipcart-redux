9d501d23b863e84938853f11fb9d29ce2fc5ae1c
krupali <krupali.lalakiya236@gmail.com>
2025-01-31 19:56:04 +0530

    flipcart project started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+export default function App() {
+  return (
+   <>
+   <h1 className='bg-slate-600'>Hello</h1>
+   <button className='bg-slate-600 text-white'>Add</button>
+   </>
+  );
+};
+  
