15c3c5b92138ed0343a4cea90541e6266b7e6f53
Krupali Lalakiya <krupali.lalakiya@gmail.com>
2025-03-07 15:55:11 +0530

    done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,7 +51,7 @@ const Header = (props) => {
               className="text-white focus:outline-none"
               onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
             >
-              <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <svg className="w-8 h-8" fill="none" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
               </svg>
             </button>
